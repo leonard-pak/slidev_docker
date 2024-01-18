3cf168c6a699ab04f2152a2261ab6040403c2017
Leonard Pak <leopak2000@gmail.com>
2024-01-18 03:53:18 +0300

    fix entrypoint and dockerfile, add password to remote control

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,18 @@
 #!/bin/sh
 
-npm config set registry https://registry.npm.taobao.org 
-
 if [ -f /slidev/slides.md ]; then
-    if [ -d /slidev/node_modules ]; then
-        npm update
+    if [ -d /slidev/package.json ]; then
+        npm ci
     else
         npm install @slidev/cli @slidev/theme-default @slidev/theme-seriph
     fi
     echo "Start slidev..."
-    
+
 else
     echo "slides.md not found in the bind mount to /slidev"
     npm install @slidev/cli @slidev/theme-default @slidev/theme-seriph
     cp -f /slidev/node_modules/@slidev/cli/template.md /slidev/slides.md
-    sed -i ':a;N;$!ba;s/GitHub"\n/GitHub"/g' /slidev/slides.md
 
 fi
 
-npm config delete registry
-
-npx slidev --remote
+npx slidev --remote $SLIDEV_PASS
